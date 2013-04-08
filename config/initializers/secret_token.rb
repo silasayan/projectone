5db501ed30df79d722b268bf28552d6fda3f842c
Sila Sayan <silasayan@gmail.com>
2013-04-08 00:39:26 -0400

    Tried restarting rails app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Projectone::Application.config.secret_token = '6a4a85f5a8989690404b1731a7be2ed4df4e90db7a196ca4dd4986ccc716a58601d1887aa12424808142a65cb3b26415243f093cc6e981337c0385e9ca788a1b'
+Projectone::Application.config.secret_token = 'c8622f8f7d8c02a3b47d9a86fa82a36bb94bd2c12da2ec105bcade4e847179fa297c42527ccc0a6df05e64479d905984afc97e74c090cbb569d19f29ffe63668'
