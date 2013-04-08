33c5714387f30ffbacf082f19923c47c49d192a7
Sila Sayan <silasayan@gmail.com>
2013-04-07 23:41:43 -0400

    Created app for Project One -analytics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectone::Application.config.secret_token = '6a4a85f5a8989690404b1731a7be2ed4df4e90db7a196ca4dd4986ccc716a58601d1887aa12424808142a65cb3b26415243f093cc6e981337c0385e9ca788a1b'
